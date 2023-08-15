59258c7451fe4ddb035726d6c5f9f7c97b9b174d
Felipe De Andrade <felipe.deam98@gmail.com>
2023-08-15 16:05:27 -0300

    Adição de PWA

--- assets/js/mudancaHorario.js
+++ assets/js/mudancaHorario.js
@@ -12,17 +12,17 @@ function updateBackgroundColor() {
   const periodoAtual = checaHora();
   const dados = getDadosPerido(periodoAtual);
 
-  const [startColor1, startColor2] = dados.cores.from; // Cor inicial (preta)
-  const [endColor1, endColor2] = dados.cores.to; // Cor final (laranja)
-  const startTime = new Date().setHours(dados.horario[0], 0, 0, 0); // 00:00
-  const endTime = new Date().setHours(dados.horario[1], 36, 0, 0); // 06:00
+  const [corInicial1, corInicial2] = dados.cores.from; // Cor inicial (preta)
+  const [corFinal1, corFinal2] = dados.cores.to; // Cor final (laranja)
+  const horaInicio = new Date().setHours(dados.horario[0], 0, 0, 0); // 00:00
+  const horaFim = new Date().setHours(dados.horario[1], 36, 0, 0); // 06:00
 
-  const currentTime = new Date().getTime();
-  const elapsedTime = Math.max(0, Math.min(currentTime - startTime, endTime - startTime));
-  const totalTime = endTime - startTime;
+  const horaAtual = new Date().getTime();
+  const elapsedTime = Math.max(0, Math.min(horaAtual - horaInicio, horaFim - horaInicio));
+  const totalTime = horaFim - horaInicio;
 
   const ratio = elapsedTime / totalTime;
-  const interpolatedColor = `linear-gradient(${interpolateColor(startColor1, endColor1, ratio)}, ${interpolateColor(startColor2, endColor2, ratio)})`;
+  const interpolatedColor = `linear-gradient(${interpolateColor(corInicial1, corFinal1, ratio)}, ${interpolateColor(corInicial2, corFinal2, ratio)})`;
 
   corpo.style.background = interpolatedColor;
 
---
+++ sw.js
@@ -0,0 +1,52 @@
+const cacheName = 'API de tradução';
+
+const versao = 2;
+
+self.addEventListener('install', function(event){
+    event.waitUntil(
+        caches.open(cacheName).then(function (cache) {
+            cache.addAll([
+                './',
+                './index.html',
+                './manifest.webmanifest',
+
+                './assets/css/reset.css',
+                './assets/css/animations.css',
+                './assets/css/styles.css',
+                './assets/css/box.css',
+                './assets/css/bandeira.css',
+
+                './assets/js/periodos.js',
+                './assets/js/mudancaHorario.js',
+
+                './API/conectaApi.js',
+                './API/conectaBandeiras.js',
+                './API/exibeOpcoes.js',
+                './API/metodoTraduzir.js',
+                './API/orderAlfabetica.js',
+
+                './assets/images/lago-01.svg',
+                './assets/images/arrow.svg',
+
+                './assets/images/bandeiras/br.svg',
+                './assets/images/bandeiras/ca.svg',
+                './assets/images/bandeiras/fr.svg',
+                './assets/images/bandeiras/it.svg',
+                './assets/images/bandeiras/jp.svg',
+                './assets/images/bandeiras/mx.svg',
+
+            ])
+        })
+    )
+    return self.skipWaiting();
+});
+
+self.addEventListener("fetch", event => {
+    event.respondWith(
+      caches.match(event.request)
+        .then(cachedResponse => {
+            return cachedResponse || fetch(event.request);
+      }
+    )
+   )
+ });
