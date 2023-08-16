c84aa1306e455d0ef5cf8b060973004a768637fc
Felipe De Andrade <felipe.deam98@gmail.com>
2023-08-16 16:42:25 -0300

    teste 3

--- API/conectaBandeiras.js
+++ API/conectaBandeiras.js
@@ -1,5 +1,5 @@
 // const endpointDaApi = 'http://localhost:3000/paises';
-const endpointDaApi = 'https://epiled.github.io/hello-world/dadosBandeiras.json';
+const endpointDaApi = 'https://epiled.github.io/hello-world/API/dadosBandeiras.json';
 
 async function listaPaises() {
   const conexao = await fetch(endpointDaApi);
